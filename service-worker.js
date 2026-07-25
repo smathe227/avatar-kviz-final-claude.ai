65a1ea0a171fad419cae16ee0e19b27d8a9ebc7b
smathe227 <smathe227@users.noreply.github.com>
2026-07-25 21:48:08 +0200

    Update: Opus5 vylepsenie + TTS 1s + TTSLíng + puzzle contain + skip safety

--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,7 @@ const CACHE_NAME = "avatar-kviz-" + VERSION;
 const ASSETS = [
   "./",
   "index.html",
-  "manifest.json",
+  "manifest.webmanifest",
   "avatar-assets/app-icon-192.png",
   "avatar-assets/app-icon-512.png",
   "avatar-assets/avatar_01.jpg",
